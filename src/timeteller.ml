5160d4bd1d2dc6a57ef18583eb735eb3aeef43c4
Darren Ldl <darrenldldev@gmail.com>
2020-09-20 16:34:39 +1000

    Progress

--- src/timeteller.ml
+++ src/timeteller.ml
@@ -1,6 +1,20 @@
+let listen_sockaddr =
+  Unix.(ADDR_INET ((inet_addr_of_string "0.0.0.0"), 9999))
+
+let respond (client_sock_addr : Unix.sockaddr)
+    ((ic, oc) : Lwt_io.input_channel * Lwt_io.output_channel) : unit Lwt.t =
+  Lwt.return ()
+
+let server : Lwt_io.server Lwt.t =
+  Lwt_io.establish_server_with_client_address
+    listen_sockaddr
+    respond
+
+let main =
+  let%lwt () = Lwt_io.printf "Starting server\n" in
+  let%lwt server = server in
+  let%lwt () = Lwt_io.printf "Shutting down server\n" in
+  Lwt_io.shutdown_server server
+
 let () =
-  Lwt_main.run begin
-    let%lwt data = Lwt_io.(read_line stdin) in
-    let%lwt () = Lwt_io.printl data in
-    Lwt.return ()
-  end
+  Lwt_main.run main
