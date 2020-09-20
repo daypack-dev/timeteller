2a5bb79eb7c9a6a4e818fd6f5e23b29e723078a2
Darren Ldl <darrenldldev@gmail.com>
2020-09-20 16:05:17 +1000

    Added lwt use

--- src/timeteller.ml
+++ src/timeteller.ml
@@ -1,1 +1,6 @@
-let () = print_endline "Hello, World!"
+let () =
+  Lwt_main.run begin
+    let%lwt data = Lwt_io.(read_line stdin) in
+    let%lwt () = Lwt_io.printl data in
+    Lwt.return ()
+  end
