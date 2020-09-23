eddc386f18b2666493935412678bba4af936583d
Darren Ldl <darrenldldev@gmail.com>
2020-09-23 16:54:49 +1000

    Adding code for sending HTTP GET to ip-api.com

--- src/timeteller.ml
+++ src/timeteller.ml
@@ -1,14 +1,28 @@
+open Lwt
+open Cohttp
+open Cohttp_lwt_unix
+
 let listen_sockaddr = Unix.(ADDR_INET (inet_addr_any, 9999))
 
-(* let resolve_time_zone_offset_s_via_ip_api_dot_com (sock_addr : Unix.sockaddr) : int Lwt.t =
- * Lwt_io.with_connection *)
+let resolve_time_zone_offset_s_via_ip_api_dot_com (sock_addr : Unix.sockaddr) : int option Lwt.t =
+  match sock_addr with
+  | Unix.ADDR_UNIX _ -> Lwt.return None
+  | Unix.ADDR_INET (addr, _port) ->
+    let addr_str = Unix.string_of_inet_addr addr in
+    let uri = Uri.of_string ("http://ip-api.com/json/" ^ addr_str) in
+    let%lwt (resp, body) = Client.get uri in
+    let%lwt body_str = Cohttp_lwt.Body.to_string body in
+    let%lwt () = Lwt_io.print body_str in
+    Lwt.return None
 
 let respond (client_sock_addr : Unix.sockaddr)
     ((ic, oc) : Lwt_io.input_channel * Lwt_io.output_channel) : unit Lwt.t =
   let%lwt input = Lwt_io.read ic in
+  let%lwt _ = resolve_time_zone_offset_s_via_ip_api_dot_com client_sock_addr in
   let%lwt () = Lwt_io.printf "Input:\n" in
   let%lwt () = Lwt_io.printf "%s\n" input in
-  let%lwt () = Lwt_io.write_line oc "abcd" in
+  let%lwt () = Lwt_io.write oc "abcd" in
+  let%lwt () = Lwt_io.flush oc in
   Lwt.return ()
 
 let server : Lwt_io.server Lwt.t =
